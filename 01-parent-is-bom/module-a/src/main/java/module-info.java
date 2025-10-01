a3b79c988fb27d060861a1487d55f9822f4b5ed9
Alexander Bernhofer <asedlmayr@web.de>
2025-10-01 08:52:29 +0200

    Maven 3.9.11: Parent as BOM
    With java modules

---
+++ 01-parent-is-bom/module-a/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module de.drunkenmasters.learning.maven.moda {
+    exports de.drunkenmasters.learning.maven.moda;
+
+    requires de.drunkenmasters.learning.maven.liba;
+    requires de.drunkenmasters.learning.maven.modb;
+    requires de.drunkenmasters.learning.maven.modc;
+}
---
+++ 01-parent-is-bom/module-c/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module de.drunkenmasters.learning.maven.modc {
+    exports de.drunkenmasters.learning.maven.modc;
+
+    requires de.drunkenmasters.learning.maven.liba;
+    requires de.drunkenmasters.learning.maven.modb;
+}
